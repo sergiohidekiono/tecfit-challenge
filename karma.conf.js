c056f2909ea71ef5513ed477d4d1a87c4ce24a5c
Sergio Hideki Ono <sergiohidekiono@gmail.com>
2022-07-28 11:40:26 -0300

    Ajustes na configuração

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/tecnofit-challenge'),
+      dir: require('path').join(__dirname, './coverage/tecfit-challenge'),
       subdir: '.',
       reporters: [
         { type: 'html' },
